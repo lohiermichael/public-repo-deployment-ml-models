e440016ed588ca0da5da9e318a03ac3090fb0fbf
Michael Lohier <lohiermichael@gmail.com>
2020-08-05 12:18:22 +0800

    Remove zip folder and test the script file

--- scripts/fetch_kaggle_large_dataset.sh
+++ scripts/fetch_kaggle_large_dataset.sh
@@ -4,15 +4,18 @@ TRAINING_DATA_URL="vbookshelf/v2-plant-seedlings-dataset"
 NOW=$(date)
 
 # We download the dataset from Kaggle
-kaggle datasets download -d $TRAINING_DATA_URL -p packages/neural_network_model/neural_network_model/datasets/ && \
+kaggle datasets download -d $TRAINING_DATA_URL -p packages/neural_network_model/neural_network_model/datasets/
 
 # We unzip the dataset
-unzip packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset && \
+unzip packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip -d packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/
+
+# Remove the zip file
+rm packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset.zip
 
 # We persist to a file:
 # 1. the URL we use
 # 2. the date of the training data
-echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/neural_network_model/neural_network_model/datasets/training_data_reference.txt && \
+echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/neural_network_model/neural_network_model/datasets/training_data_reference.txt
 
 # Rename the folder that has an apostrophe
 mkdir -p "./packages/neural_network_model/neural_network_model/datasets/v2-plant-seedlings-dataset/Shepherds Purse"  && \
